779e51b21b4d5c0a50a7d6c32338d768f63bd39d
Thaddeus Anderson <thaddeusa@gmail.com>
2019-12-08 12:03:22 -0700

    initial

---
+++ init.sh
@@ -0,0 +1,8 @@
+# install home brew
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+# install 1password, keybase, and chrome
+
+brew bundle install --file=config/Brewfile_init
