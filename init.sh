31a01ea3bcc5be197918a365a1588e602c0b67b5
Thaddeus Anderson <thaddeusa@gmail.com>
2023-12-17 16:38:43 -0600

    update to new brew install method

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 # install home brew
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install 1password, keybase, and chrome
 
